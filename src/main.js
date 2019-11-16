7ed1a9c806abbbb4f38bb9c2f05adebf44efbd20
matheusfaggi <matheusfaggi.ht@gmail.com>
2019-11-16 02:57:37 -0300

    escopo todo-list selfedit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-new */
 import api from './api';
 
 class App {
   constructor() {
     this.tasks = this.getTasks();
+
+    this.formEl = document.getElementById('task-form');
+    this.registrerHandlers();
+  }
+
+  registrerHandlers() {
+    this.formEl.onsubmit = (event) => this.addRepository(event);
   }
 
   async getTasks() {
