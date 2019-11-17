2e2a511220dc76e4f7067f92484ba238efc17b66
matheusfaggi <matheusfaggi.ht@gmail.com>
2019-11-17 04:47:06 -0300

    delete utilizando api implementado

--- src/api.js
+++ src/api.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 class Api {
   constructor() {
     const ApiNode = axios.create({
-      baseURL: 'localhost:3000',
+      baseURL: 'http://localhost:3000',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
     });
     this.ApiNode = ApiNode;
   }
+  
 }
 export default new Api().ApiNode;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -16,28 +16,64 @@ class TodoList extends Component {
   state = {
     newTask: '',
     tasks: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
     ],
   };
+  constructor() {
+    super();
+    this.setTasks(); 
+  }
   handleInputChange  = (e) => {
     const { value } = e.target;
     this.setState({ newTask: value});
+    
   }
-  handleSubmit = (e) => {
-    const { tasks, newTask } = this.state
+  postTask = async (e) => {
+    const params = {
+        task_description: this.state.newTask,
+    }
+    const response  = await api.post('/task',params);
+    console.log(response)
+  }
+  deleteTask = async (task) => {
+    const dbTasks = await this.getTasks();
+    console.log(task)
+    const id = dbTasks.filter(item=>item.task_description === task).map(item=>item.id);
+    api.delete('/task',{
+      data: {
+        id
+      }
+    })
+  }
+  getTasks = async () => {
+    const response = await api.get('/tasks')
+    .then(response => response.data.map((item)=>item));
+    return response;
+  }
+
+  setTasks = async () => {
+    const tasksDb = await this.getTasks();
+    const dbTasks = tasksDb.map((item)=>item.task_description);
+    this.setState({
+      tasks: [ ...dbTasks]
+    })
+  }
+  handleSubmit =  (e) => {
     e.preventDefault();
+    this.postTask(e);
+    const { tasks, newTask } = this.state;
     this.setState({ 
       tasks: [...tasks, newTask] ,
       newTask: ''
     })
   }
-  handleDelete = (task) => {  
+  handleDelete = (task) => {
+    this.deleteTask(task);
     this.setState({ tasks: this.state.tasks.filter(t => t !== task)})
   }
-  render() {
+  
+  render () {
     const { tasks, newTask } = this.state;
+
     return(
       <form onSubmit={this.handleSubmit} id="form-task"> 
         <Input 
