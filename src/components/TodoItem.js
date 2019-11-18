cbb921bbc591ab8b492b32ebedd923b00ff16b09
matheusfaggi <matheusfaggi.ht@gmail.com>
2019-11-18 19:32:33 -0300

    Ajustes master branch

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
-import Delete from "@material-ui/icons/Delete";
-import Edit from "@material-ui/icons/Edit";
-import FormLabel from '@material-ui/core/FormLabel';
-import IconButton from "@material-ui/core/IconButton";
-import Checkbox from "@material-ui/core/Checkbox";
-import './Style.css';
+import React, { Component } from 'react'
+import Delete from '@material-ui/icons/Delete'
+import Edit from '@material-ui/icons/Edit'
+import FormLabel from '@material-ui/core/FormLabel'
+import IconButton from '@material-ui/core/IconButton'
+import Checkbox from '@material-ui/core/Checkbox'
+import './Style.css'
 
-class TodoItem extends Component{
-  static defaultProps = {
-    isNew: false,
-  }
-  render(){
-    const {  onDelete, task, isNew,onEdit } = this.props;
-    
-    return (
-    <li className="task">
-      <div className="task-description">
-        {!isNew ? <Checkbox></Checkbox> : null}
-        <FormLabel>{task}</FormLabel>
-      </div>
-      <div className="tools-task">
-        <IconButton onClick={onEdit}>
-          {!isNew ? <Edit></Edit> : null}
-        </IconButton>
+class TodoItem extends Component {
+    static defaultProps = {
+        isNew: false,
+    }
+    render() {
+        const { onDelete, task, isNew, onEdit } = this.props
 
-        <IconButton onClick={onDelete}>
-          {!isNew ? <Delete></Delete> : null}
-        </IconButton>
-      </div>
-        
-    </li>
-    
-    );
-  }
+        return (
+            <li className="task">
+                <div className="task-description">
+                    {!isNew ? <Checkbox></Checkbox> : null}
+                    <FormLabel>{task}</FormLabel>
+                </div>
+                <div className="tools-task">
+                    <IconButton onClick={onEdit}>
+                        {!isNew ? <Edit></Edit> : null}
+                    </IconButton>
+
+                    <IconButton onClick={onDelete}>
+                        {!isNew ? <Delete></Delete> : null}
+                    </IconButton>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default TodoItem
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,123 +9,103 @@ import { deepOrange, blueGrey } from '@material-ui/core/colors'
 import api from '../api'
 
 class TodoList extends Component {
-  static defaultProp = {
-    task: 'Olá'
-  }
-  state = {
-    newTask: '',
-    tasks: [
-    ],
-  };
-  constructor() {
-    super();
-    this.setTasks(); 
-  }
-  handleInputChange  = (e) => {
-    const { value } = e.target;
-    this.setState({ newTask: value});
-    
-  }
-  postTask = async (e) => {
-    const params = {
-        task_description: this.state.newTask,
+    static defaultProp = {
+        task: 'Olá',
     }
-    const response  = await api.post('/task',params);
-  }
-  deleteTask = async (task) => {
-    const dbTasks = await this.getTasks();
-    const id = dbTasks.filter(item=>item.task_description === task).map(item=>item.id);
-    api.delete('/task',{
-      data: {
-        id
-      }
-    })
-  }
-  getTasks = async () => {
-    const response = await api.get('/tasks')
-    .then(response => response.data.map((item)=>item));
-    return response;
-  }
+    state = {
+        newTask: '',
+        tasks: [],
+    }
+    constructor() {
+        super()
+        this.setTasks()
+    }
+    handleInputChange = e => {
+        const { value } = e.target
+        this.setState({ newTask: value })
+    }
+    postTask = async e => {
+        const params = {
+            task_description: this.state.newTask,
+        }
+        const response = await api.post('/task', params)
+    }
+    deleteTask = async task => {
+        const dbTasks = await this.getTasks()
+        const id = dbTasks
+            .filter(item => item.task_description === task)
+            .map(item => item.id)
+        api.delete('/task', {
+            data: {
+                id,
+            },
+        })
+    }
+    getTasks = async () => {
+        const response = await api
+            .get('/tasks')
+            .then(response => response.data.map(item => item))
+        return response
+    }
 
-  setTasks = async () => {
-    const tasksDb = await this.getTasks();
-    const dbTasks = tasksDb.map((item)=>item.task_description);
-    this.setState({
-      tasks: [ ...dbTasks]
-    })
-  }
-  handleSubmit =  (e) => {
-    e.preventDefault();
-    this.postTask(e);
-    const { tasks, newTask } = this.state;
-    this.setState({ 
-      tasks: [...tasks, newTask] ,
-      newTask: ''
-    })
-  }
-  handleDelete = (task) => {
-    this.deleteTask(task);
-    this.setState({ tasks: this.state.tasks.filter(t => t !== task)})
-  }
-  handleEdit = (task) => {
-    console.log(task)
-  }
-  
-  render () {
-    const { tasks, newTask } = this.state;
-
-    return(
-      <form onSubmit={this.handleSubmit} id="form-todo">
-        <div id="form-task">
-          <div id="input-task">
-            <Input 
-              type="text" 
-              onChange={this.handleInputChange}
-              value={newTask}
-            />
-          </div>
-          <div id="btn-task">
-            <Button 
-              variant="contained" 
-              color="primary" 
-              type="submit"
-              id="btn-task"
-              >
-              Add task
-            </Button>
-          </div>
-          
-        </div>
-        <ul id="list-task">
-          {tasks.map(task=>(
-          <TodoItem 
-            key={task} 
-            task={task} 
-            onDelete={()=>this.handleDelete(task)}
-            onEdit={()=>this.handleEdit(task)}
-          />
-          ))}
-          <TodoItem 
-          isNew={true}
-          task={newTask}/> 
-        </ul>
-      </form>
-    );
+    setTasks = async () => {
+        const tasksDb = await this.getTasks()
+        const dbTasks = tasksDb.map(item => item.task_description)
+        this.setState({
+            tasks: [...dbTasks],
+        })
+    }
+    handleSubmit = e => {
+        e.preventDefault()
+        this.postTask(e)
+        const { tasks, newTask } = this.state
+        this.setState({
+            tasks: [...tasks, newTask],
+            newTask: '',
+        })
+    }
+    handleDelete = task => {
+        this.deleteTask(task)
+        this.setState({ tasks: this.state.tasks.filter(t => t !== task) })
+    }
+    handleEdit = task => {
+        console.log(task)
+    }
 
     render() {
         const { tasks, newTask } = this.state
 
         return (
             <form onSubmit={this.handleSubmit} id="form-todo">
+                <div id="form-task">
+                    <div id="input-task">
+                        <Input
+                            type="text"
+                            onChange={this.handleInputChange}
+                            value={newTask}
+                        />
+                    </div>
+                    <div id="btn-task">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            id="btn-task"
+                        >
+                            Add task
+                        </Button>
+                    </div>
+                </div>
                 <ul id="list-task">
                     {tasks.map(task => (
                         <TodoItem
                             key={task}
                             task={task}
                             onDelete={() => this.handleDelete(task)}
+                            onEdit={() => this.handleEdit(task)}
                         />
                     ))}
-                    <TodoItem isNew={true} newTask={newTask} />
+                    <TodoItem isNew={true} task={newTask} />
                 </ul>
             </form>
         )
